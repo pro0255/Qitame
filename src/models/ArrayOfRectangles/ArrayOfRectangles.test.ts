5e484573d6fbcfe9a6812b9dc0329559cba31f82
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-09-13 21:57:29 +0200

    test: adding tests for split which modifies correctly URL

---
+++ src/models/ArrayOfRectangles/ArrayOfRectangles.test.ts
@@ -0,0 +1,30 @@
+import { ArrayOfRectangles } from './ArrayOfRectangles';
+
+describe('ArrayOfRectangles', () => {
+  it('when the input consists of the numbers "[100,45,21,60,24]," the contents are parsed into an array containing the specified numbers', () => {
+    const arrayOfRectangles = new ArrayOfRectangles('[100,45,21,60,24]');
+
+    expect(arrayOfRectangles.contents).toEqual([100, 45, 21, 60, 24]);
+  });
+  describe('split', () => {
+    it('whenever the input is "[100,45,21,60,24]" and it is split, the resulting array becomes [33, 67, 45, 21, 60, 24]', () => {
+      const arrayOfRectangles = new ArrayOfRectangles('[100,45,21,60,24]');
+      const rectangle100 = arrayOfRectangles.rectangles[0];
+
+      arrayOfRectangles.split(rectangle100);
+
+      expect(arrayOfRectangles.contents).toEqual([33, 67, 45, 21, 60, 24]);
+    });
+    it('whenever the input is "[100,45,21,60,24]" and it is split, the URL is updated to [33, 67, 45, 21, 60, 24]', () => {
+      // Object.defineProperty(global.window, 'location', {
+      //   value: new URL('https://ematiq.is.great.com/solutionView/[100,45,21,60,24]'),
+      // });
+      const arrayOfRectangles = new ArrayOfRectangles('[100,45,21,60,24]');
+      const rectangle100 = arrayOfRectangles.rectangles[0];
+
+      arrayOfRectangles.split(rectangle100);
+
+      expect(global.window.location.href).toEqual('http://localhost/solutionView/[33,67,45,21,60,24]');
+    });
+  });
+});
