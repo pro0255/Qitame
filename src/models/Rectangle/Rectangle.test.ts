a4146888de7ca2481892cf6fede07cd13628b3c1
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-09-13 20:39:52 +0200

    test: other use cases

--- src/models/Rectangle/Rectangle.test.ts
+++ src/models/Rectangle/Rectangle.test.ts
@@ -10,5 +10,21 @@ describe('Rectangle', () => {
       expect(roundedOneThird.content).toBe(15);
       expect(rest.content).toBe(31);
     });
+    it("when rectangle with S = '15' is split, result is rectangles with S = '5' and S = '10'", () => {
+      const rectangle = new Rectangle(46);
+
+      const { roundedOneThird, rest } = rectangle.split();
+
+      expect(roundedOneThird.content).toBe(15);
+      expect(rest.content).toBe(31);
+    });
+    it("when rectangle with S = '10' is split, result is rectangles with S = '3' and S = '7'", () => {
+      const rectangle = new Rectangle(46);
+
+      const { roundedOneThird, rest } = rectangle.split();
+
+      expect(roundedOneThird.content).toBe(15);
+      expect(rest.content).toBe(31);
+    });
   });
 });
