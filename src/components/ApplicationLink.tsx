45f72084ec2202ae80d8d445461d48c948178051
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-09-14 17:57:00 +0200

    feat: adding logic for styles

--- src/components/ApplicationLink.tsx
+++ src/components/ApplicationLink.tsx
@@ -1,6 +1,16 @@
 import { ComponentProps } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const ApplicationLink = (props: ComponentProps<typeof Link>) => {
-  return <Link {...props} className={'p-5 hover:bg-gray-200 focus:bg-gray-200 focus:ring-0 outline-none'} />;
+  const location = useLocation();
+
+  // Not stable but good enough for the purpose
+  const isCurrent = props.to === '/' ? location.pathname === '/' : location.pathname.includes(props.to.toString());
+
+  return (
+    <Link
+      {...props}
+      className={`p-5 hover:bg-gray-200 focus:bg-gray-100 focus:ring-0 outline-none ${isCurrent ? 'bg-gray-100' : ''}`}
+    />
+  );
 };
