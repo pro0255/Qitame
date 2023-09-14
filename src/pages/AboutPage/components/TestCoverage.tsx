632f51bc07db7d488e399508d03d87015ee39a0f
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-09-14 23:06:59 +0200

    refactor: change list

--- src/pages/AboutPage/components/TestCoverage.tsx
+++ src/pages/AboutPage/components/TestCoverage.tsx
@@ -2,6 +2,6 @@ import { GrayText } from '../../../ds/GrayText';
 
 export const TestCoverage = () => {
   return (
-    <GrayText>The application benefits from thorough test coverage, ensuring its reliability and robustness.</GrayText>
+    <GrayText>The application benefits from thorough test coverage, ensuring its reliability and robustness</GrayText>
   );
 };
