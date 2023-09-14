0b71d7522d7f22c1d55d1880ea03fd682d4837ca
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-09-14 23:17:09 +0200

    feat: full width

--- src/layouts/CenterLayout.tsx
+++ src/layouts/CenterLayout.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const CenterLayout = ({ children }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
+    <div className="max-w-full overflow-auto flex flex-col items-center justify-center w-full h-full">
       <footer className={'sticky top-0 w-full z-10 bg-white'}>
         <Navigation />
       </footer>
