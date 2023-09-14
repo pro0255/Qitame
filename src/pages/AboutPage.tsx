f17010e3dc6f31005b557c26e0d999dd30aa5bf0
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-09-14 18:14:00 +0200

    feat: initialization of about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,5 +1,13 @@
 import { CenterLayout } from '../layouts/CenterLayout';
 
 export const AboutPage = () => {
-  return <CenterLayout>This is About</CenterLayout>;
+  return (
+    <CenterLayout>
+      <div className={'flex w-full h-full mt-20 justify-center'}>
+        <ul className={'flex flex-col items-center'}>
+          <li>This page was created by Vojtech Prokop.</li>
+        </ul>
+      </div>
+    </CenterLayout>
+  );
 };
