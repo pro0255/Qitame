e421ef921c5f0cc5cd29cb8449b987d7a8d6125a
Prokop, Vojtech <vojtech.prokop@ataccama.com>
2023-09-14 18:09:24 +0200

    feat: adding welcome page

--- src/components/HiToEmatiq.tsx
+++ src/components/HiToEmatiq.tsx
@@ -1,3 +1,11 @@
 export const HiToEmatiq = () => {
-  return <h3>Hello to Ematiq from Vojta</h3>;
+  return (
+    <p className={'text-lg font-normal text-gray-500'}>
+      Hello to{' '}
+      <a href="https://www.ematiq.com/" target="_blank">
+        Ematiq
+      </a>{' '}
+      from Vojta
+    </p>
+  );
 };
--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,17 +1,22 @@
 import reactLogo from '../assets/react.svg';
+import ematiqLogo from '../assets/ematiq.png';
+
 import { HiToEmatiq } from '../components/HiToEmatiq';
 import { CenterLayout } from '../layouts/CenterLayout';
 
 export const WelcomePage = () => {
   return (
     <CenterLayout>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+      <div className={'w-full h-full flex flex-col justify-center items-center'}>
+        <img width={'20%'} height={'20%'} src={ematiqLogo} alt="Ematiq logo" />
+        <a
+          className={'mt-20 w-full h-[20%] flex flex-col justify-center items-center'}
+          href="https://react.dev"
+          target="_blank"
+        >
+          <img width={'5%'} height={'5%'} src={reactLogo} alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+        <h1 className={'text-4xl font-extrabold leading-none tracking-tight text-gray-900'}>Vite + React</h1>
         <HiToEmatiq />
       </div>
     </CenterLayout>
